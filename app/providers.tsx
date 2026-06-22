42e340864204c4a12069998d6d477974a0ea67a0
Parikshit Sankhyan <parikshitsankhyan0024@gmail.com>
2026-06-22 10:09:53 +0530

    Fix: use React.ComponentProps for ThemeProvider types (next-themes)

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,9 +1,6 @@
+import React from 'react';
 import { ThemeProvider as NextThemeProvider } from 'next-themes';
-import { ThemeProviderProps } from 'next-themes/dist/types';
 
-export function ThemeProvider({
-    children,
-    ...props
-}: Readonly<ThemeProviderProps>) {
+export function ThemeProvider({ children, ...props }: React.ComponentProps<typeof NextThemeProvider>) {
     return <NextThemeProvider {...props}>{children}</NextThemeProvider>;
 }
